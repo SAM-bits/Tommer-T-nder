9b236d6cf17fb801a461dc61ba315c79130f4ac6
Saif <Saif@BOOK-LBSNKSKQ3G>
2026-02-19 13:15:07 +0100

    Add project files.

---
+++ Tommer Tønder/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Tommer_Tønder
+{
+    internal class Program
+    {
+        static void Volume(double radius , double højde ) 
+        {
+            // det her er udregning af volumen formlen det er p*radius^2*h 
+            double volumen = Math.PI * Math.Pow(radius, 2) * højde;
+            Console.WriteLine($"{volumen:F3}");
+
+
+            return;
+
+        }
+
+        static void Overfladearealet(double radius, double højde)
+        {
+            // det her er udregning af volumen formlen det er p*radius^2*h 
+            double overfladearealet = 2 * Math.PI * radius * (radius + højde);
+            Console.WriteLine($"{overfladearealet:F3}");
+
+
+            return;
+
+        }
+        static void Main(string[] args)
+        {
+            /* udregne volumen = V = p *r2 * h
+            overfladearealet for en cylinder : A=2*p*r^2+2*p*r*h
+
+
+             */
+            // double input = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Volume(10 , 5);
+            Overfladearealet(10, 5);
+            
+            
+        }
+    }
+}
